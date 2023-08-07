cd8735b516583154a50d95590c7a05b4852cfd6d
iliketwertles <67071538+iliketwertles@users.noreply.github.com>
2023-08-06 22:16:39 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
+use std::process::Stdio;
+use std::os::unix::process::CommandExt;
 use std::fs;
+use std::env;
 use std::io::{stdout, Write};
 extern crate pam;
 extern crate ini;
 use ini::Ini;
 use std::process::Command;
-use users::get_user_by_name;
-use std::os::unix::process::CommandExt;
-
 fn main() {
     // get  desktops
     let desktops = fs::read_dir("/usr/share/wayland-sessions").unwrap();
 
-    // define variables for later use, also ^ 
+    // define variables for later use, also ^
     let mut username = String::new();
-    let mut choice = String::new(); 
+    let mut choice = String::new();
     let mut _count = 0;
     let mut desktops_path = Vec::new();
 
     for desktop in desktops {
         desktops_path.push(desktop.unwrap().path());
     }
+    let env_path = env!("PATH");
+    let env_xdg_dir = env!("XDG_RUNTIME_DIR");
     println!("Availible desktops: ");
     for (i, desk) in desktops_path.iter().enumerate() {
         println!("{desk} [{i}]", desk = desk.display().to_string().replace("/usr/share/wayland-sessions/", "").replace(".desktop", ""));
@@ -27,7 +29,7 @@ fn main() {
 
     // adding newline for looks
     println!();
-    // save username 
+    // save username
     print!("Username: ");
     stdout().flush().unwrap();
     let _b1 = std::io::stdin().read_line(&mut username).unwrap();
@@ -48,26 +50,35 @@ fn main() {
     let data = Ini::load_from_file_noescape(path).unwrap();
     let section = data.section(Some("Desktop Entry")).unwrap();
     let exec = section.get("Exec").unwrap();
-    let mut exec_split = exec.split_whitespace();
+    let exec_split = exec.split_whitespace();
     let name = section.get("Name").unwrap();
 
-    // PAM shit
+    // PAM shit, attempting to log user in
     let mut auth = pam::Authenticator::with_password("tdm").unwrap();
-    auth.get_handler().set_credentials(&username, password);
+    auth.get_handler().set_credentials(username.trim(), password);
     if auth.authenticate().is_ok() && auth.open_session().is_ok() {
         println!("Successfully opened a session!");
     }
     else {
         println!("Authentication failed =/");
     }
+    auth.close_on_drop = false;
 
-    // launching 
-    let user = get_user_by_name(&username).unwrap();
-    println!("Now launching {}", name); 
-    let _error = Command::new(exec_split.next().unwrap())
-        .args(exec_split.next())
-        .uid(user.uid())
-        .gid(user.primary_group_id())
+
+    // launching selected DE/WM
+    // setting path for some reason
+    println!("Now launching {}", name);
+    let exec_args: Vec<_> = exec_split.collect();
+    let _error = Command::new(exec_args[0])
+        .args(&exec_args[1..])
+        .env("PATH", env_path)
+        .env("XDG_RUNTIME_DIR", env_xdg_dir)
+        .env("XDG_DATA_DIRS", "/usr/share")
+        .env("XDG_CONFIG_DIRS", "/etc/xdg")
+        .uid(1000)
+        .gid(1000)
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
         .exec();
 
 }
